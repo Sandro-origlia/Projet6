099bb1645867ce529fd83380b246944944bf9553
Sandro Origlia <sandrale4@hotmail.fr>
2023-10-24 17:30:48 +0200

    recuperation_projet

---
+++ FrontEnd/script.js
@@ -0,0 +1,14 @@
+fetch("http://localhost:5678/api/works");
+.then((response) => response.json());
+.then(response2 => console.log(reponse2))
+
+
+
+
+
+
+
+
+let card = document.createElement("div");
+
+let photo = document.getElementById("1");
